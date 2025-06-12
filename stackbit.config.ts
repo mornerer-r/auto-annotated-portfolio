dd5c534e0e9dcf30214b107a2842b845ae9e588e
mornerer-r <ukilinc27@outlook.com>
2025-06-12 18:58:36 +0300

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -26,3 +26,6 @@ const config = defineStackbitConfig({
     styleObjectModelName: 'ThemeStyle'
 });
 export default config;
+<div data-sb-object-id="1">
+  <h1 data-sb-field-path="title">My Page Title</h1>
+</div>
